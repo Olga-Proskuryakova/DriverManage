37652c3fe9622dda0f9e9e39c7bc612185cdffe8
Olga-Proskuryakova <115976455+Olga-Proskuryakova@users.noreply.github.com>
2024-02-14 12:48:15 +0300

    1. deleted main

--- src/test/java/Authorization_Test.java
+++ src/test/java/Authorization_Test.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import tools.WaitTools;
 
 
